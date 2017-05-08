d77c71aa87792c3c4a8126cfe15fea587acd5c99
VahanAper <asryanvahandev@gmail.com>
2017-05-08 13:29:53 +0400

    app middleware initial setup and nodemon setup

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
