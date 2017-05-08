5b30e1bea5d664f5b6f336b762529cda4dcd1c52
VahanAper <asryanvahandev@gmail.com>
2017-05-08 13:40:34 +0400

    router initial setup

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-const app = express();
+const router = require('./router');
 
+// App Setup
+const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
+// Server Setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
