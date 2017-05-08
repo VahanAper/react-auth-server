3d83afea947cd969d85edc9146a68533485a5e3a
VahanAper <asryanvahandev@gmail.com>
2017-05-08 15:20:04 +0400

    mongodb and mongoose initial setup

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const router = require('./router');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 // App Setup
 const app = express();
