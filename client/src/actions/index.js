4576116e80702bca4f6f8ee1295f57f5db87a449
VahanAper <asryanvahandev@gmail.com>
2017-05-10 11:47:04 +0400

    Signout component, action creator and reducer implemnted

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 
-import { AUTH_USER, AUTH_ERROR } from './types';
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -28,5 +28,12 @@ export function signinUser({ email, password }) {
         // Show an error to the user
         dispatch(authError('Bad login info.'));
       });
+  };
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token');
+  return {
+    type: UNAUTH_USER,
   };
 }
