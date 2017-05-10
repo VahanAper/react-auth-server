19258e4d37464967da211af9f51e205883fbc724
VahanAper <asryanvahandev@gmail.com>
2017-05-10 11:31:46 +0400

    Header component refactored considering auth state

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,17 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <li className="nav-item">
+          <Link className="nav-link" to="/signout">Sign Out</Link>
+        </li>
+      );
+    }
+    return [
+      <li className="nav-item" key={1}>
+        <Link className="nav-link" to="/signin">Sign In</Link>
+      </li>,
+      <li className="nav-item" key={2}>
+        <Link className="nav-link" to="/signup">Sign Up</Link>
+      </li>,
+    ];
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Home</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign In
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  authenticated: PropTypes.bool,
+};
+Header.defaultProps = {
+  authenticated: false,
+};
+
+const mapStateToProps = function (state) {
+  return {
+    authenticated: state.auth.authenticated,
+  };
+};
+
+export default connect(mapStateToProps)(Header);
