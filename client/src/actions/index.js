966360ad163620580ea78a18fdee0c5a1940f87c
VahanAper <asryanvahandev@gmail.com>
2017-05-10 10:47:18 +0400

    auth action creator and reducer added

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   return function (dispatch) {
     // Submit email and password to the server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then((response) => {
+        // Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // Redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch();
   };
 }
